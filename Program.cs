3e7e43b8558cc918ded98f2ea1eaebd20764dbf6
HamplusTech <51116379+HamplusTech@users.noreply.github.com>
2021-06-11 15:48:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Hello_Universe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Universe");
+            Console.ReadKey();
+            //iterate over command line
+            // and print them out
+            for (int arg = 0; arg < args.Length; arg++)
+            {
+                Console.WriteLine("arg{0}:{1}", arg, args[arg]);
+            }
+         }
+    }
+}
